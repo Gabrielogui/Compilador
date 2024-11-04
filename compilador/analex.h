dfcfce6e355e3dfa097c82e7f1bbf62b06797445
Gabrielogui <gabrielcsro@gmail.com>
2024-11-04 20:16:04 -0300

    Compilador Projeto
    - Criação do header
    - Criação do main

---
+++ compilador/analex.h
@@ -0,0 +1,25 @@
+#ifndef ANALEX
+#define ANALEX
+
+#define TAM_MAX_LEXEMA 31
+
+enum TOKEN_CAT {PR = 1, ID, CT_I, CT_R, CT_C, LT, SN, FIM_ARQ};
+
+/* Onde: ID: Identificador, SN: Sinal; CT_I: Constante numérica inteira */
+
+enum SINAIS {ATRIB = 1, ADICAO, SUBTRACAO, MULTIPLIC, DIVISAO, ABRE_PAR, FECHA_PAR}; // Sinais válidos da linguagem
+
+typedef struct {
+    enum TOKEN_CAT cat; // deve receber uma das constantes de enum TOKEN_CAT
+    union { // parte variável do registro
+        int codigo; // para tokens das categorias SN
+        char lexema[TAM_MAX_LEXEMA]; // cadeia de caracteres que corresponde ao nome do token da cat. ID
+        int valor_i; // valor da constante inteira em tokens da cat. CT_I
+        float valor_r;
+        char c;
+    };
+} TOKEN; // Tipo TOKEN
+#endif
+
+int contLinhas = 1;
+
---
+++ compilador/main.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "analex.h"
+
+#define TAM_LEXEMA 50
+#define TAM_NUM 20
+
+// |=======|  DECLARAÇÃO DAS FUNÇÕES |=======|
+TOKEN analise_lexica(FILE*);
+
+// |=======| MAIN |=======|
+int main()
+{
+    printf("oi");
+    return 0;
+}
+
+// |=======| FUNÇÕES |=======|
+TOKEN analise_lexica(FILE *fd){
+    // VARIÁVEIS DAS FUNÇÕES:
+    int estado;
+    char lexema[TAM_LEXEMA] = "";
+    int tamL = 0;
+    char digitos[TAM_NUM] = "";
+    int tamD = 0;
+
+    // WHILE TRUE:
+    while(1){
+        char c = getc(fd);
+        switch(estado){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+            case 16:
+                break;
+            case 17:
+                break;
+            case 18:
+                break;
+            case 19:
+                break;
+            case 20:
+                break;
+            case 21:
+                break;
+            case 22:
+                break;
+            case 23:
+                break;
+            case 24:
+                break;
+            case 25:
+                break;
+            case 26:
+                break;
+            case 27:
+                break;
+            case 28:
+                break;
+            case 29:
+                break;
+            case 30:
+                break;
+            case 31:
+                break;
+            case 32:
+                break;
+            case 33:
+                break;
+            case 34:
+                break;
+            case 35:
+                break;
+            case 36:
+                break;
+            case 37:
+                break;
+            case 38:
+                break;
+            case 39:
+                break;
+            case 40:
+                break;
+            case 41:
+                break;
+            case 42:
+                break;
+            case 43:
+                break;
+            case 44:
+                break;
+            case 45:
+                break;
+            case 46:
+                break;
+            case 47:
+                break;
+            case 48:
+                break;
+        }
+    }
+}
